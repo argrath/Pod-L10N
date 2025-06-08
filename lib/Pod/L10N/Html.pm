755061b0f9e1868995bd969083ec61c904db53ac
SHIRAKATA Kentaro <argrath@ub32.org>
2025-06-09 01:16:03 +0900

    Checking in changes prior to tagging of version 1.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 00820bb..a85a36f 100755
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl module Pod::L10N
     
     {{$NEXT}}
     
    +1.08 2025-06-08T16:14:46Z
    +
     	- upgrade upstream to 1.33 (5.36.0-based)
     	- fix test failure on Pod::Simple 3.46 or later

--- lib/Pod/L10N/Html.pm
+++ lib/Pod/L10N/Html.pm
@@ -2,7 +2,7 @@ package Pod::L10N::Html;
 use strict;
 use Exporter 'import';
 
-our $VERSION = '1.07';
+our $VERSION = '1.08';
 $VERSION = eval $VERSION;
 our @EXPORT = qw(pod2htmll10n htmlify);
 our @EXPORT_OK = qw(anchorify relativize_url);
